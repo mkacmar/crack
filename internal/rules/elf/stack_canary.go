613bc4c67c50e7f333043948d1cfac82a5d310e3
Martin Kačmar <martin@kacmar.sk>
2026-01-19 00:20:06 +0100

    Make detection more robust for stack_canary
    Do not rely on error being returned when reading symbols.

--- internal/rules/elf/stack_canary.go
+++ internal/rules/elf/stack_canary.go
@@ -29,30 +29,31 @@ func (r StackCanaryRule) Feature() model.FeatureAvailability {
 }
 
 func (r StackCanaryRule) Execute(f *elf.File, info *model.ParsedBinary) model.RuleResult {
-	hasStackCanary := false
-	symbols, err := f.Symbols()
-	if err != nil {
-		dynsyms, err := f.DynamicSymbols()
-		if err == nil {
-			symbols = dynsyms
-		}
-	}
+	symbols, _ := f.Symbols()
+	dynsyms, _ := f.DynamicSymbols()
 
 	for _, sym := range symbols {
 		if strings.Contains(sym.Name, "__stack_chk_fail") ||
 			strings.Contains(sym.Name, "__stack_smash_handler") ||
 			strings.Contains(sym.Name, "__intel_security_cookie") {
-			hasStackCanary = true
-			break
+			return model.RuleResult{
+				State:   model.CheckStatePassed,
+				Message: "Stack canary protection is enabled",
+			}
 		}
 	}
 
-	if hasStackCanary {
-		return model.RuleResult{
-			State:   model.CheckStatePassed,
-			Message: "Stack canary protection is enabled",
+	for _, sym := range dynsyms {
+		if strings.Contains(sym.Name, "__stack_chk_fail") ||
+			strings.Contains(sym.Name, "__stack_smash_handler") ||
+			strings.Contains(sym.Name, "__intel_security_cookie") {
+			return model.RuleResult{
+				State:   model.CheckStatePassed,
+				Message: "Stack canary protection is enabled",
+			}
 		}
 	}
+
 	return model.RuleResult{
 		State:   model.CheckStateFailed,
 		Message: "Stack canary protection is NOT enabled",
