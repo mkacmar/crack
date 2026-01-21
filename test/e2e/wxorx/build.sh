c467647507d4daf49f9bb228fc3f229f3531022b
Martin Kačmar <martin@kacmar.sk>
2026-01-21 21:45:26 +0100

    Generate golden binaries for wxorx

---
+++ test/e2e/wxorx/build.sh
@@ -0,0 +1,21 @@
+#!/bin/sh
+set -ex
+
+ARCH=$1
+SRC=test/e2e/testdata/main.c
+mkdir -p binaries
+
+. test/e2e/testdata/log-env.sh
+
+gcc -o binaries/${ARCH}-gcc-wxorx $SRC
+gcc -z execstack -o binaries/${ARCH}-gcc-execstack $SRC
+gcc -shared -fPIC -o binaries/${ARCH}-gcc-shared-wxorx $SRC
+gcc -shared -fPIC -z execstack -o binaries/${ARCH}-gcc-shared-execstack $SRC
+
+clang -o binaries/${ARCH}-clang-wxorx $SRC
+clang -z execstack -o binaries/${ARCH}-clang-execstack $SRC
+clang -shared -fPIC -o binaries/${ARCH}-clang-shared-wxorx $SRC
+clang -shared -fPIC -z execstack -o binaries/${ARCH}-clang-shared-execstack $SRC
+
+ls -la binaries/
+
